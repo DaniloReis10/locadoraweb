26f36f8a4b2779ae4977d5be9156b9b61d0e9841
igor-lopes <igor_olf@hotmail.com>
2016-03-31 04:08:14 -0300

    Correção da validação do formulário e CEP

--- equipe- igor-emanuel/js/cad_cliente.js
+++ equipe- igor-emanuel/js/cad_cliente.js
@@ -1,30 +1,42 @@
-function validateForm(){
+function validateForm() {
 
     var cep = document.forms["client-form"]["zipcode"].value;
     var pwd = document.forms["client-form"]["pwd"].value;
     var pwd_conf = document.forms["client-form"]["pwd-conf"].value;
 
-    if(pwd == pwd_conf){
+    if (pwd == pwd_conf) {
         clearForm();
         return true;
     }
 
-    getZipCode(cep);
 
 }
 
-function getZipCode(code){
-    script.src = '//viacep.com.br/ws/'+ code + '/json/?callback=meu_callback';
-
+function getDados(dados) {
+    alert("here");
+    document.getElementById('rua').value = (dados.logradouro);
+    document.getElementById('bairro').value = (dados.bairro);
+    document.getElementById('cidade').value = (dados.localidade);
+    document.getElementById('estado').value = (dados.estado);
+    document.getElementById('cidade').value = (dados.cidade);
 }
 
-function clearForm(){
-    document.getElementById('name').value=("");
-    document.getElementById('last-name').value=("");
-    document.getElementById('sex').value=("");
-    document.getElementById('pwd').value=("");
-    document.getElementById('pwd-conf').value=("");
-    document.getElementById('area').value=("");
-    document.getElementById('number').value=("");
-    document.getElementById('zipcode').value=("");
+function checkCode(code) {
+    var api = "http://api.postmon.com.br/v1/cep/";
+    var url = api.concat(code);
+    var script = document.createElement('script');
+    //insertReply("igor");
+    script.src = 'http://api.postmon.com.br/v1/cep/' + code + '?callback=getDados';
+    document.body.appendChild(script);
+}
+
+function clearForm() {
+    document.getElementById('name').value = ("");
+    document.getElementById('last-name').value = ("");
+    document.getElementById('sex').value = ("");
+    document.getElementById('pwd').value = ("");
+    document.getElementById('pwd-conf').value = ("");
+    document.getElementById('area').value = ("");
+    document.getElementById('number').value = ("");
+    document.getElementById('zipcode').value = ("");
 }
