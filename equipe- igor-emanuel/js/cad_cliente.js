639d1435766f7ee211ccfa192f839740d5443f7d
igor-lopes <igor_olf@hotmail.com>
2016-03-31 14:52:55 -0300

    Formulários organizados

--- equipe- igor-emanuel/js/cad_cliente.js
+++ equipe- igor-emanuel/js/cad_cliente.js
@@ -1,42 +1,31 @@
-function validateForm() {
+function checkPWD(pwd, pwd_conf) {
 
-    var cep = document.forms["client-form"]["zipcode"].value;
     var pwd = document.forms["client-form"]["pwd"].value;
     var pwd_conf = document.forms["client-form"]["pwd-conf"].value;
-
-    if (pwd == pwd_conf) {
-        clearForm();
-        return true;
+    if (pwd != pwd_conf) {
+        document.forms["client-form"]["pwd-conf"].setCustomValidity('Senha diferente');
+        return false;
     }
 
-
+    return true;
 }
 
 function getDados(dados) {
-    alert("here");
-    document.getElementById('rua').value = (dados.logradouro);
-    document.getElementById('bairro').value = (dados.bairro);
-    document.getElementById('cidade').value = (dados.localidade);
-    document.getElementById('estado').value = (dados.estado);
-    document.getElementById('cidade').value = (dados.cidade);
+    if (!("erro" in dados)) {
+        document.getElementById('street').value = (dados.logradouro);
+        document.getElementById('area').value = (dados.bairro);
+        document.getElementById('city').value = (dados.localidade);
+        document.getElementById('state').value = (dados.uf);
+    } else{
+        document.forms["client-form"]["zipcode"].setCustomValidity('CEP não encontrado');
+    }
 }
 
 function checkCode(code) {
+
     var api = "http://api.postmon.com.br/v1/cep/";
     var url = api.concat(code);
     var script = document.createElement('script');
-    //insertReply("igor");
-    script.src = 'http://api.postmon.com.br/v1/cep/' + code + '?callback=getDados';
+    script.src = 'http://viacep.com.br/ws/' + code + '/json/?callback=getDados';
     document.body.appendChild(script);
-}
-
-function clearForm() {
-    document.getElementById('name').value = ("");
-    document.getElementById('last-name').value = ("");
-    document.getElementById('sex').value = ("");
-    document.getElementById('pwd').value = ("");
-    document.getElementById('pwd-conf').value = ("");
-    document.getElementById('area').value = ("");
-    document.getElementById('number').value = ("");
-    document.getElementById('zipcode').value = ("");
 }
