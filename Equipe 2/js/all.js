7be3a3d83339f489ce540a952b99818f166e5a3d
leonel <leonelrc31@gmail.com>
2016-04-05 15:03:45 -0300

    Novos campos do cadastro de veículo e datepiker no campo de data de compra.

--- Equipe 2/js/all.js
+++ Equipe 2/js/all.js
@@ -1,15 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#accordion").accordion();
 
     jQuery.extend(jQuery.validator.messages, {
         required: "Campo obrigatório."
     });
 
-    jQuery.validator.addMethod("date", function(value, element) {
+    jQuery.validator.addMethod("date", function (value, element) {
         return this.optional(element) || /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(value);
     }, "Informe um data válida");
 
     $("#create-form").validate();
 
+    $("#fabricacao").mask("99/99");
     $("#compra").mask("99/99/9999");
+    $("#compra").datepicker({
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+        dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        nextText: 'Próximo',
+        prevText: 'Anterior'
+    });
 });
